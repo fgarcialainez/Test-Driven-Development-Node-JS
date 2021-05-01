d15aa8222fc621584bb7a5822357b819789a037e
Felix Garcia Lainez <fgarcialainez@gmail.com>
2021-05-01 17:47:35 +0200

    Included additional validations for email, username and password fields

--- nodeapi-tdd/__test__/UserRegister.spec.js
+++ nodeapi-tdd/__test__/UserRegister.spec.js
@@ -118,19 +118,31 @@ describe('User Registration', () => {
   );
 
   it.each`
-    field         | expectedMessage
-    ${'username'} | ${'Username can not be null'}
-    ${'email'}    | ${'Email can not be null'}
-    ${'password'} | ${'Password can not be null'}
+    field         | value              | expectedMessage
+    ${'username'} | ${null}            | ${'Username can not be null'}
+    ${'username'} | ${'usr'}           | ${'Must have min 4 and max 32 characters'}
+    ${'username'} | ${'a'.repeat(33)}  | ${'Must have min 4 and max 32 characters'}
+    ${'email'}    | ${null}            | ${'Email can not be null'}
+    ${'email'}    | ${'mail.com'}      | ${'Email is not valid'}
+    ${'email'}    | ${'user.mail.com'} | ${'Email is not valid'}
+    ${'email'}    | ${'user@mail'}     | ${'Email is not valid'}
+    ${'password'} | ${null}            | ${'Password can not be null'}
+    ${'password'} | ${'P4ssw'}         | ${'Password must be at least 6 characters'}
+    ${'password'} | ${'alllowercase'}  | ${'Password must have at least 1 uppercase, 1 lowercase letter and 1 number'}
+    ${'password'} | ${'ALLUPPERCASE'}  | ${'Password must have at least 1 uppercase, 1 lowercase letter and 1 number'}
+    ${'password'} | ${'1234567890'}    | ${'Password must have at least 1 uppercase, 1 lowercase letter and 1 number'}
+    ${'password'} | ${'lowerandUPPER'} | ${'Password must have at least 1 uppercase, 1 lowercase letter and 1 number'}
+    ${'password'} | ${'lower4nd5667'}  | ${'Password must have at least 1 uppercase, 1 lowercase letter and 1 number'}
+    ${'password'} | ${'UPPER44444'}    | ${'Password must have at least 1 uppercase, 1 lowercase letter and 1 number'}
   `(
-    'Returns validationErrors in body when $field is null',
-    async ({ field, expectedMessage }) => {
+    'Returns $expectedMessage when $field is $value.',
+    async ({ field, value, expectedMessage }) => {
       const user = {
         username: 'user1',
         email: 'user1@email.com',
         password: 'P4ssword',
       };
-      user[field] = null;
+      user[field] = value;
 
       const response = await postUser(user);
       expect(response.body.validationErrors[field]).toBe(expectedMessage);
