b67ce799abd14ccc4790a8e3fb35e8f2a274f55d
Felix Garcia Lainez <fgarcialainez@gmail.com>
2021-05-16 13:52:59 +0200

    Include log to show the application version

--- nodeapi-tdd/src/shared/logger.js
+++ nodeapi-tdd/src/shared/logger.js
@@ -3,7 +3,9 @@ const { createLogger, transports, format } = require('winston');
 const customFormat = format.combine(
   format.timestamp(),
   format.printf((info) => {
-    return `${info.timestamp} [${info.level.toUpperCase().padEnd(7)}] : ${info.message}`;
+    return `${info.timestamp} [${info.level.toUpperCase().padEnd(7)}] : ${
+      info.message
+    }`;
   })
 );
 
