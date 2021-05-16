2c7eb67edeefa611a84486171897e461454a5a68
Felix Garcia Lainez <fgarcialainez@gmail.com>
2021-05-16 13:20:39 +0200

    Included sequelize-cli package and created db migrations and seeders

--- nodeapi-tdd/index.js
+++ nodeapi-tdd/index.js
@@ -1,27 +1,9 @@
-const bcrypt = require('bcrypt');
 const app = require('./src/app');
 const sequelize = require('./src/config/database');
-const User = require('./src/models/User');
 const TokenService = require('./src/services/TokenService');
 
-// Function to add an initial set of users in the system
-const addUsers = async (activeUserCount, inactiveUserCount = 0) => {
-  const hash = await bcrypt.hash('P4ssword', 10);
-
-  for (let i = 0; i < activeUserCount + inactiveUserCount; i++) {
-    await User.create({
-      username: `user${i + 1}`,
-      email: `user${i + 1}@mail.com`,
-      inactive: i >= activeUserCount,
-      password: hash,
-    });
-  }
-};
-
 // Synchronize the ORM with database
-sequelize.sync({ force: true }).then(async () => {
-  await addUsers(25);
-});
+sequelize.sync();
 
 // Clean tokens
 TokenService.scheduleCleanup();
